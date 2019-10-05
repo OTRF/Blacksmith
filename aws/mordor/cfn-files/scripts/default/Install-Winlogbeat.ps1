fcfe7c1ae68c573539ea3ce64b32affa8a9ec025
Cyb3rWard0g <rpberto@38f9d3618a64.ant.amazon.com>
2019-10-04 21:17:54 -0400

    Update Install-Winlogbeat.ps1

--- aws/mordor/cfn-files/scripts/default/Install-Winlogbeat.ps1
+++ aws/mordor/cfn-files/scripts/default/Install-Winlogbeat.ps1
@@ -25,46 +25,42 @@ write-Host "Decompressing $OutputFile .."
 $file = (Get-Item $ZipFile).Basename
 expand-archive -path $Zipfile -DestinationPath "C:\Program Files\"
 if (!(Test-Path "C:\Program Files\$file")){ write-Host "$ZipFile could not be decompressed successfully.. "; break }
-#Renaming Folder & File
+
+# Renaming Folder & File
 write-Host "Renaming folder from C:\Program Files\$file to C:\Program Files\Winlogbeat .."
 Rename-Item "C:\Program Files\$file" "C:\Program Files\Winlogbeat" -Force
 
+# Backing up default Winlogbeat configuration
 write-Host "Renaming file from C:\Program Files\Winlogbeat\winlogbeat.yml to C:\Program Files\Winlogbeat\winlogbeat.backup .."
 Rename-Item "C:\Program Files\Winlogbeat\winlogbeat.yml" "C:\Program Files\Winlogbeat\winlogbeat.backup" -Force
 
+# Download WInlogbeat Config
 write-Host "Downloading Winlogbeat config.."
 $WinlogbeatFile = "C:\Program Files\Winlogbeat\winlogbeat.yml"
 $wc.DownloadFile($WinlogbeatConfigUrl, $WinlogbeatFile)
 if (!(Test-Path $WinlogbeatFile)){ write-Host "File $WinlogbeatFile does not exists.. "; break }
 
-try {
-    write-Host "Installing Winlogbeat Service.."
-    & "C:\Program Files\Winlogbeat\install-service-winlogbeat.ps1"
-    
-    write-Host "Starting Winlogbeat service.."
-    Start-Service winlogbeat
+# Installing Winlogbeat Service
+write-Host "Installing Winlogbeat Service.."
+& "C:\Program Files\Winlogbeat\install-service-winlogbeat.ps1"
 
-    write-Host "Verifying if Winlogbeat is running.."
-    $ServiceName = 'winlogbeat'
-    $arrService = Get-Service -Name $ServiceName
+write-Host "Starting Winlogbeat service.."
+Start-Service winlogbeat
 
-    while ($arrService.Status -ne 'Running')
+write-Host "Verifying if Winlogbeat is running.."
+$ServiceName = 'winlogbeat'
+$arrService = Get-Service -Name $ServiceName
+
+while ($arrService.Status -ne 'Running')
+{
+    Start-Service $ServiceName
+    write-host $arrService.status
+    write-host 'Service starting'
+    Start-Sleep -seconds 5
+    $arrService.Refresh()
+    if ($arrService.Status -eq 'Running')
     {
-        Start-Service $ServiceName
-        write-host $arrService.status
-        write-host 'Service starting'
-        Start-Sleep -seconds 5
-        $arrService.Refresh()
-        if ($arrService.Status -eq 'Running')
-        {
-            Write-Host 'Service is now Running'
-        }
-
+        Write-Host 'Service is now Running'
     }
-    write-Host "Winlogbeat is running.."
 }
-catch {
-    $ErrorMessage = $_.Exception.Message
-    write-Host "Winlogbeat installation failed with ERROR: $ErrorMessage "
-    break
-}
+write-Host "Winlogbeat is running.."
