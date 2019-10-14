67b5c3c6ff103c3c0fb9a9ecd563055cee6d09c8
Cyb3rWard0g <rpberto@38f9d3618a64.ant.amazon.com>
2019-10-14 00:56:33 -0400

    Update New-NetworkShares.ps1

--- aws/mordor/cfn-files/scripts/default/New-NetworkShares.ps1
+++ aws/mordor/cfn-files/scripts/default/New-NetworkShares.ps1
@@ -17,17 +17,17 @@ $folders = @("secrets", "gates")
 
 foreach($folder in $folders)
 {
-    New-Item "C:\$folder" –type directory
+    New-Item C:\$folder –type directory
     if($folder -Like "secrets")
     {
         New-SMBShare –Name "secrets" –Path "C:\secrets" `
-            –ContinuouslyAvailable `
+            –ContinuouslyAvailable $true `
             –FullAccess "$DomainName1\Domain Admins"
     }
     else
     {
         New-SMBShare –Name "gates" –Path "C:\gates" `
-            –ContinuouslyAvailable `
+            –ContinuouslyAvailable $true `
             –FullAccess "$DomainName1\Domain Admins" `
             -ReadAccess "$DomainName1\Domain Users"
     }
