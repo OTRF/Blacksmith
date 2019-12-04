1fe614a30b74203c0306575f9dc25bdb736bf41e
Cyb3rWard0g <rpberto@38f9d3618a64.ant.amazon.com>
2019-12-04 18:39:29 -0500

    added nxlog scripts
    - installation
    - configs

---
+++ aws/mordor/cfn-files/scripts/default/Install-Shipper.ps1
@@ -0,0 +1,96 @@
+# Author: Roberto Rodriguez (@Cyb3rWard0g)
+# License: GPL-3.0
+
+# References:
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("Winlogbeat","Nxlog")]
+    [string]$ShipperAgent,
+
+    [Parameter(Mandatory=$true)]
+    [string]$ConfigUrl
+)
+
+if($ShipperAgent -eq "Winlogbeat")
+{
+    $URL = "https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.4.0-windows-x86_64.zip"
+}
+else
+{
+    $Url = "https://nxlog.co/system/files/products/files/348/nxlog-ce-2.10.2150.msi"
+}
+
+$OutputFile = Split-Path $URL -leaf
+$NewFile = "c:\cfn\scripts\$outputFile"
+
+# Download Installer
+write-Host "Downloading $OutputFile .."
+$wc = new-object System.Net.WebClient
+$wc.DownloadFile($Url, $NewFile)
+if (!(Test-Path $NewFile)){ write-Host "File $NewFile does not exists.. "; break }
+
+if($ShipperAgent -eq "Winlogbeat")
+{
+    # Unzip file
+    write-Host "Decompressing $OutputFile .."
+    $file = (Get-Item $NewFile).Basename
+    expand-archive -path $NewFile -DestinationPath "C:\Program Files\"
+    if (!(Test-Path "C:\Program Files\$file")){ write-Host "$NewFile could not be decompressed successfully.. "; break }
+
+    # Renaming Folder & File
+    write-Host "Renaming folder from C:\Program Files\$file to C:\Program Files\Winlogbeat .."
+    Rename-Item "C:\Program Files\$file" "C:\Program Files\Winlogbeat" -Force
+
+    # Backing up default Winlogbeat configuration
+    write-Host "Renaming file from C:\Program Files\Winlogbeat\winlogbeat.yml to C:\Program Files\Winlogbeat\winlogbeat.backup .."
+    Rename-Item "C:\Program Files\Winlogbeat\winlogbeat.yml" "C:\Program Files\Winlogbeat\winlogbeat.backup" -Force
+
+    # Installing Winlogbeat Service
+    write-Host "Installing Winlogbeat Service.."
+    & "C:\Program Files\Winlogbeat\install-service-winlogbeat.ps1"
+
+    $shipperConfig = "C:\Program Files\Winlogbeat\winlogbeat.yml"
+    $ServiceName = 'winlogbeat'
+}
+else
+{
+    # Installing nxlog
+    write-Host "Installing nxlog .."
+    & c:\Windows\system32\msiexec /passive /qn /i $NewFile
+
+    # Download nxlog Config
+    write-Host "waiting for nxlog folder to exist .."
+    while (!(Test-Path "C:\Program Files (x86)\nxlog")) { Start-Sleep 5 }
+
+    # Renaming File
+    write-Host "Renaming original nxlog config .."
+    while (!(Test-Path "C:\Program Files (x86)\nxlog\conf\nxlog.conf")) { Start-Sleep 5 }
+    Rename-Item "C:\Program Files (x86)\nxlog\conf\nxlog.conf" "C:\Program Files (x86)\nxlog\conf\nxlog.backup.conf" -Force
+
+    $shipperConfig = "C:\Program Files (x86)\nxlog\conf\nxlog.conf"
+    $ServiceName = 'nxlog'
+}
+
+# Download shipper config
+write-Host "Downloading shipper config.."
+$wc.DownloadFile($ConfigUrl, $shipperConfig)
+if (!(Test-Path $shipperConfig)){ write-Host "File $shipperConfig does not exists.. "; break }
+
+# Installing Service
+$arrService = Get-Service -Name $ServiceName
+
+while ($arrService.Status -ne 'Running')
+{
+    Start-Service $ServiceName
+    write-host $arrService.status
+    write-host "Service $ServiceName starting"
+    Start-Sleep -seconds 5
+    $arrService.Refresh()
+    if ($arrService.Status -eq 'Running')
+    {
+        Write-Host "Service $ServiceName is now Running"
+    }
+}
+write-Host "$ServiceName is running.."
