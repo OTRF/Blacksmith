c0e6e421ff6b430275513abdbc00ca8094d8dd86
Roberto Rodriguez <rrodri0622@gmail.com>
2020-06-24 01:41:10 -0400

    Updated variables for a modular approach

--- resources/scripts/bash/Install-C2s.sh
+++ resources/scripts/bash/Install-C2s.sh
@@ -6,7 +6,6 @@
 # *********** log tagging variables ***********
 INFO_TAG="[INSTALLATION-INFO]"
 ERROR_TAG="[INSTALLATION-ERROR]"
-DATE_TIME=`date "+%Y-%m-%d %H:%M:%S"`
 
 # *********** Set Log File ***************
 LOGFILE="/var/log/C2s-install.log"
@@ -20,8 +19,7 @@ usage(){
     echo "Usage: $0 [option...]" >&2
     echo
     echo "   -r         run a specific C2 server (empire or covenant or caldera)"
-    echo "   -u         C2 admin user name (optional)"
-    echo "   -p         C2 admin password (optional)"
+    echo "   -i         C2 IP address (it could be the redirector too)"
     echo "   -h         help menu"
     echo
     echo "Examples:"
@@ -31,13 +29,12 @@ usage(){
 }
 
 # ************ Command Options **********************
-while getopts r:u:p:h option
+while getopts r:i:h option
 do
     case "${option}"
     in
         r) RUN_C2=$OPTARG;;
-        u) ADMIN_USER_NAME=$OPTARG;;
-        p) ADMIN_PASSWORD=$OPTARG;;
+        i) IP_ADDDRESS=$OPTARG;;
         h) usage;;
         \?) usage;;
         :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
@@ -57,6 +54,7 @@ case $RUN_C2 in
     covenant);;
     caldera);;
     metasploit);;
+    shad0w);;
     *)
         echo "$ERROR_TAG Not a valid C2 option. Valid Options: empire or covenant or caldera"
         usage
@@ -76,35 +74,26 @@ mkdir /opt/Impacket
 cd /opt/Impacket && curl -s https://api.github.com/repos/ropnop/impacket_static_binaries/releases/latest | grep "browser_download_url.*linux_x86_64" | cut -d '"' -f 4 | wget -qi -
 
 # *********** Running default C2 Selected ***********
+echo "$INFO_TAG Deploying $RUN_C2 .."
 if [[ $RUN_C2 == "covenant" ]]; then
     # *********** Installing Covenant ***************
-    echo "$INFO_TAG Setting up Covenant.."
     git clone --recurse-submodules https://github.com/cobbr/Covenant /opt/Covenant >> $LOGFILE 2>&1
     cd /opt/Covenant/Covenant && docker build -t covenant . >> $LOGFILE 2>&1
 
-    echo "$INFO_TAG Running Covenant by default.."
     docker run -d -it -p 7443:7443 -p 80:80 -p 443:443 -p 8443-8500:8443-8500 --name covenant -v /opt/Covenant/Covenant/Data:/app/Data covenant >> $LOGFILE 2>&1  
 elif [[ $RUN_C2 == "empire" ]]; then
     # *********** Installing Empire ***************
-    echo "$INFO_TAG Setting up Empire.."
-    git clone https://github.com/BC-SECURITY/Empire /opt/Empire
+    git clone https://github.com/BC-SECURITY/Empire /opt/Empire $LOGFILE 2>&1
     cd /opt/Empire && docker build -t empire . >> $LOGFILE 2>&1
     docker create -v /opt/Empire --name data empire >> $LOGFILE 2>&1
 
-    echo "$INFO_TAG Running Empire by default.."
-    cd /opt/Empire && docker run -d -it -p 80:80 -p 443:443 -p 8443-8500:8443-8500 --name empire --volumes-from data empire /bin/bash >> $LOGFILE 2>&1
+    docker run -d -it -p 80:80 -p 443:443 -p 8443-8500:8443-8500 --name empire --volumes-from data empire /bin/bash >> $LOGFILE 2>&1
 elif [[ $RUN_C2 == "caldera" ]]; then
     # *********** Installing Caldera ***************
-    echo "$INFO_TAG Setting up Caldera.."
-    mkdir /opt/Caldera
-    curl -L https://raw.githubusercontent.com/hunters-forge/Blacksmith/master/resources/scripts/docker/caldera/2.6.6-040520/docker-compose-caldera.yml -o /opt/Caldera/docker-compose-caldera.yml >> $LOGFILE 2>&1
-    echo "$INFO_TAG Running Caldera by default.."
-
-    #U Updating Caldera dmin password
-    export CALDERA_RED_ADMIN_PASSWORD=$ADMIN_PASSWORD
-    export CALDERA_RED_ADMIN_NAME=$ADMIN_USER_NAME
-
-    docker-compose -f /opt/Caldera/docker-compose-caldera.yml up --build -d
+    git clone https://github.com/Cyb3rWard0g/docker-caldera /opt/caldera $LOGFILE 2>&1
+    cd /opt/caldera && docker build -t caldera . $LOGFILE 2>&1
+    
+    docker run -t -it -p 8888:8888 -p 7010:7010/tcp -p 7010:7010/udp -p 7012:7012 --name caldera caldera $LOGFILE 2>&1
 elif [[ $RUN_C2 == "metasploit" ]]; then
     # *********** Installing Metasploit ***************
     docker image pull metasploitframework/metasploit-framework
@@ -112,4 +101,14 @@ elif [[ $RUN_C2 == "metasploit" ]]; then
     # Run manually:
     # docker run --rm -it -p 443:443 -v "/opt/attack-platform:/tmp/attack-platform" metasploitframework/metasploit-framework ./msfconsole
     # docker run --rm -it -p 8443:8443 -v "/opt/attack-platform:/tmp/attack-platform" metasploitframework/metasploit-framework ./msfconsole
+elif [[ $RUN_C2 == "shad0w" ]]; then
+    # *********** Installing Shad0w ***************
+    git clone --recurse-submodules https://github.com/bats3c/shad0w.git /opt/shad0w >> $LOGFILE 2>&1
+    cd /opt/shad0w && sudo ./shad0w install >> $LOGFILE 2>&1
+    
+    # *********** Creating PowerShell Payload ***********
+    ./shad0w beacon -p x64/windows/secure/static -H $IP_ADDDRESS -f psh -o /tmp/beacon.ps1 >> $LOGFILE 2>&1
+
+    # shad0w beacon -p x64/windows/secure/static -H 192.168.1.1 -f psh -o beacon.ps1
+    # shad0w listen
 fi
