f1ee6951ff5a0e9314accbb5c97aba973b6640e9
Roberto Rodriguez <rrodri0622@gmail.com>
2020-07-29 17:53:16 -0400

    Updated Azure WIn10 Desktop Wallpaper

--- templates/azure/Win10/scripts/initialSetup.ps1
+++ templates/azure/Win10/scripts/initialSetup.ps1
@@ -1,8 +1,14 @@
 # Author: Roberto Rodriguez (@Cyb3rWard0g)
 # License: GPL-3.0
 
+# Move Desktop Wallpaper
+Move-Item -Path .\otr.png -Destination C:\ProgramData\otr.png
+
+# Custom Settings applied
 & .\Prepare-Box.ps1
 
+# Set Windows Audit Policies
 & .\Enable-WinAuditCategories.ps1
 
+# Set Audit Rules from Set-AuditRule project
 & .\Set-SACLs.ps1
