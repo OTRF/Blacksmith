129abd3ca02d464d63ae7ad53d5aedce426439c9
Roberto Rodriguez <rrodri0622@gmail.com>
2020-08-02 21:47:20 -0400

    Updated Azure Win10-DC templates

---
+++ templates/azure/Win10-DC/scripts/Set-Initial-Settings.ps1
@@ -0,0 +1,37 @@
+# Author: Roberto Rodriguez (@Cyb3rWard0g)
+# License: GPL-3.0
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [string]$ServerAddresses,
+
+    [Parameter(Mandatory=$false)]
+    [switch]$SetDC
+)
+
+# Custom Settings applied
+& .\Prepare-Box.ps1
+
+# Windows Security Audit Categories
+if ($SetDC){
+    & .\Enable-WinAuditCategories.ps1 -SetDC
+}
+else{
+    & .\Enable-WinAuditCategories.ps1
+}
+
+# PowerShell Logging
+& .\Enable-PowerShell-Logging.ps1
+
+# Set SACLs
+& .\Set-SACLs.ps1
+
+# Setting static IP and DNS server IP
+if ($ServerAddresses)
+{
+    & .\Set-StaticIP.ps1 -ServerAddresses $ServerAddresses
+}
+
+# Set Wallpaper
+& .\Set-WallPaper.ps1
