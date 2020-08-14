7b3038722405545043e0e652cddabb3cbad82a83
Roberto Rodriguez <rrodri0622@gmail.com>
2020-08-14 11:02:55 -0400

    Update Set-Up-FW.sh

--- templates/azure/Win10-PAN-FW/scripts/Set-Up-FW.sh
+++ templates/azure/Win10-PAN-FW/scripts/Set-Up-FW.sh
@@ -56,6 +56,7 @@ fi
 
 # Set FW Creds
 FW_CREDS="$ADMIN_USER:$ADMIN_PASSWORD"
+echo "$INFO_TAG ussing PWD $FW_CREDS.." >> $LOGFILE 2>&1
 
 # *********** Wait for PAN FW ***************
 until curl --silent -k -X GET curl -k -X GET "https://$PRIVATE_IP/api/?type=keygen&user=$ADMIN_USER&password=$ADMIN_PASSWORD" --output /dev/null; do
@@ -67,6 +68,7 @@ done
 echo "$INFO_TAG Getting API Key.." >> $LOGFILE 2>&1
 API_RESPONSE=$(curl --silent -k -X GET curl -k -X GET "https://$PRIVATE_IP/api/?type=keygen&user=$ADMIN_USER&password=$ADMIN_PASSWORD")
 API_KEY=$(echo $API_RESPONSE | sed -e 's,.*<key>\([^<]*\)</key>.*,\1,g')
+echo "$INFO_TAG got this api $API_KEY.." >> $LOGFILE 2>&1
 
 # Get Admin Password-hash
 echo "$INFO_TAG Getting Password Hash.." >> $LOGFILE 2>&1
