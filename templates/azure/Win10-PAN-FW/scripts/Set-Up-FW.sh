6e7c6ec9b1fc5d931a1a92eecdba4df8e724ce15
Roberto Rodriguez <rrodri0622@gmail.com>
2020-08-14 03:39:45 -0400

    Update Set-Up-FW.sh

--- templates/azure/Win10-PAN-FW/scripts/Set-Up-FW.sh
+++ templates/azure/Win10-PAN-FW/scripts/Set-Up-FW.sh
@@ -59,21 +59,25 @@ FW_CREDS="$ADMIN_USER:$ADMIN_PASSWORD"
 
 # *********** Wait for PAN FW ***************
 until curl --silent -k -X GET curl -k -X GET "https://$PRIVATE_IP/api/?type=keygen&user=$ADMIN_USER&password=$ADMIN_PASSWORD" --output /dev/null; do
-    echo "$INFO_TAG Waiting for PAN FW to be up.."
+    echo "$INFO_TAG Waiting for PAN FW to be up.." >> $LOGFILE 2>&1
     sleep 5
 done
 
 # Get API Key
-API_RESPONSE=$(curl --silent -k -X GET curl -k -X GET "https://$PRIVATE_IP/api/?type=keygen&user=$ADMIN_USER&password=$ADMIN_PASSWORD" --output /dev/null)
+echo "$INFO_TAG Getting API Key.." >> $LOGFILE 2>&1
+API_RESPONSE=$(curl --silent -k -X GET curl -k -X GET "https://$PRIVATE_IP/api/?type=keygen&user=$ADMIN_USER&password=$ADMIN_PASSWORD")
 API_KEY=$(echo $API_RESPONSE | sed -e 's,.*<key>\([^<]*\)</key>.*,\1,g')
 
 # Get Admin Password-hash
+echo "$INFO_TAG Getting Password Hash.." >> $LOGFILE 2>&1
 PW_HASH_RESPONSE=$(curl --silent -k -u $FW_CREDS -X GET "https://$PRIVATE_IP/api/?type=op&cmd=<request><password-hash><password>$ADMIN_PASSWORD</password></password-hash></request>")
 PW_HASH=$(echo $PW_HASH_RESPONSE | sed -e 's,.*<phash>\([^<]*\)</phash>.*,\1,g')
 
 # Update Azure Sample XML Config (Username & Password-Hash)
-sed -i "s/DEMO-USER/${ADMIN_USER}/g" azure-sample.xml
-sed -i "s/DEMO-PASSWORD-HASH/${PW_HASH}/g" azure-sample.xml
+echo "$INFO_TAG Updating username and password for XML config.." >> $LOGFILE 2>&1
+sed -i "s|DEMO-USER|${ADMIN_USER}|g" azure-sample.xml >> $LOGFILE 2>&1
+sed -i "s|DEMO-PASSWORD-HASH|${PW_HASH}|g" azure-sample.xml >> $LOGFILE 2>&1
 
 # Set up PAN FW
-python Config-FW.py $API_KEY $PRIVATE_IP
+echo "$INFO_TAG Executing Config-PW script.." >>$LOGFILE 2>&1
+python Config-FW.py $API_KEY $PRIVATE_IP >> $LOGFILE 2>&1
