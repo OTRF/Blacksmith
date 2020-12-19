ed22d8a1ec29d1b1c9fc78716f31ed10689dada5
Roberto Rodriguez <rrodri0622@gmail.com>
2020-12-19 07:42:37 -0500

    Create Update-RemoteTask.ps1

---
+++ resources/scripts/powershell/misc/Update-RemoteTask.ps1
@@ -0,0 +1,47 @@
+# Author: Roberto Rodriguez (@Cyb3rWard0g)
+# License: GPL-3.0
+
+# References:
+function Update-RemoteTask {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$ComputerName,
+
+        [Parameter(Mandatory=$false)]
+        [switch]$FolderName,
+
+        [Parameter(Mandatory=$false)]
+        [switch]$TaskName,
+
+        [Parameter(Mandatory=$false)]
+        [switch]$Executable,
+
+        [Parameter(Mandatory=$false)]
+        [switch]$Arguments
+    )
+
+    # connect to Task Scheduler:
+    $service = New-Object -ComObject Schedule.Service
+    $service.Connect($ComputerName)
+
+    # Get task folder that contains tasks:
+    $folder = $service.GetFolder($FolderName)
+
+    # Enumerate Specific Task
+    $task = $folder.GetTask($TaskName)
+
+    # get task definition and change it (i.e Arguments)
+    $taskdefinition = $task.Definition
+    $taskdefinition.Actions | ForEach-Object {$_.Path = $Executable}
+    $taskdefinition.Actions | ForEach-Object {$_.Arguments = $Arguments}
+
+    # Flags:
+    # 4 = UPDATE
+    # 6 = CREATE_UPDATE
+    #
+    # LogonType:
+    # 5 = Indicates that a Local System, Local Service, or Network Service account is being used as a security context to run the task.
+    $folder.RegisterTaskDefinition($task.Name, $taskdefinition, 4, "System", $null, 5)
+}
