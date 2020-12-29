dcf5685e084ff452fb61d1b8417a47e33801794e
Roberto Rodriguez <rrodri0622@gmail.com>
2020-12-29 06:18:07 -0500

    Update Install-DSC-Modules.ps1

--- resources/scripts/powershell/active-directory/DSC/AD-ADFS-Trust/Install-DSC-Modules.ps1
+++ resources/scripts/powershell/active-directory/DSC/AD-ADFS-Trust/Install-DSC-Modules.ps1
@@ -1,41 +1,38 @@
 #Requires -Version 5
 
-function Install-DSC-Modules
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [ValidateSet("DC","ADFS",'Endpoint')]
+    [string]$SetupType,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateSet('TrustedSigned','SelfSigned')]
+    [string]$CertificateType
+
+)
+Set-ExecutionPolicy Unrestricted -Force
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Install-Module -Name ActiveDirectoryDsc -RequiredVersion 6.0.1
+Install-Module -Name NetworkingDsc -RequiredVersion 8.2.0
+Install-Module -Name xPSDesiredStateConfiguration -RequiredVersion 9.1.0
+Install-Module -Name ComputerManagementDsc -RequiredVersion 8.4.0
+
+if ($SetupType -eq 'DC')
 {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [ValidateSet("DC","ADFS",'Endpoint')]
-        [string]$SetupType,
+    Install-Module -Name xDnsServer -RequiredVersion 1.16.0.0
+    Install-Module -Name xSmbShare -Force
+    Install-Module -Name MSOnline -Force
+    Install-Module -Name AzureAD -Force
 
-        [Parameter(Mandatory=$false)]
-        [ValidateSet('TrustedSigned','SelfSigned')]
-        [string]$CertificateType
-
-    )
-    Set-ExecutionPolicy Unrestricted -Force
-
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-    Install-Module -Name ActiveDirectoryDsc -RequiredVersion 6.0.1
-    Install-Module -Name NetworkingDsc -RequiredVersion 8.2.0
-    Install-Module -Name xPSDesiredStateConfiguration -RequiredVersion 9.1.0
-    Install-Module -Name ComputerManagementDsc -RequiredVersion 8.4.0
-
-    if ($SetupType -eq 'DC')
+    if ($CertificateType -eq 'SelfSigned')
     {
-        Install-Module -Name xDnsServer -RequiredVersion 1.16.0.0
-        Install-Module -Name xSmbShare -Force
-        Install-Module -Name MSOnline -Force
-        Install-Module -Name AzureAD -Force
-
-        if ($CertificateType -eq 'SelfSigned')
-        {
-            Install-Module -Name ActiveDirectoryCSDsc -RequiredVersion 5.0.0
-            Install-Module -Name CertificateDsc -RequiredVersion 5.0.0
-        }
+        Install-Module -Name ActiveDirectoryCSDsc -RequiredVersion 5.0.0
+        Install-Module -Name CertificateDsc -RequiredVersion 5.0.0
     }
 }
