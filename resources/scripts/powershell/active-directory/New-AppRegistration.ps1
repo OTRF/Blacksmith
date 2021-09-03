7aa8ea2e1e75587476320363b9d7f14ac0efad14
Roberto Rodriguez <rrodri0622@gmail.com>
2021-09-03 15:27:38 -0400

    updated new app registration script

--- resources/scripts/powershell/active-directory/New-AppRegistration.ps1
+++ resources/scripts/powershell/active-directory/New-AppRegistration.ps1
@@ -1,7 +1,7 @@
 function New-AppRegistration {
     <#
     .SYNOPSIS
-    A PowerShell wrapper around the Azure CLI to create/register a new Azure AD application and its respective service principal.
+    A PowerShell wrapper around Az PowerShell and the Microsoft Graph API to create/register a new Azure AD application and its respective service principal.
     
     Author: Roberto Rodriguez (@Cyb3rWard0g)
     License: MIT
@@ -17,6 +17,9 @@ function New-AppRegistration {
     .PARAMETER NativeApp
     Switch to register an application which can be installed on a user's device or computer.
 
+    .PARAMETER SignInAudience
+    Specifies the Microsoft accounts that are supported for the current application. The possible values are: AzureADMyOrg, AzureADMultipleOrgs, AzureADandPersonalMicrosoftAccount (default), and PersonalMicrosoftAccount
+
     .PARAMETER IdentifierUris
     Space-separated unique URIs that Azure AD can use for this app.
 
@@ -26,9 +29,6 @@ function New-AppRegistration {
     .PARAMETER AddSecret
     Switch to create add credentials to the application.
 
-    .PARAMETER DisableImplicitGrantFlowOAuth2
-    Switch to update the application by specifying the property oauth2AllowIdTokenImplicitFlow and value false.
-
     .PARAMETER UseV2AccessTokens
     Switch to set application to use V2 access tokens.
 
@@ -37,6 +37,9 @@ function New-AppRegistration {
 
     .PARAMETER AssignAppRoleToUser
     Use thisparameter to assign an app role to a service principal. Example: wardog@domain.onmicrosoft.com.
+
+    .PARAMETER ConnectAs
+    What security context you are running the script as. It could be User or ManagedIdentity.
 
     .LINK
     https://docs.microsoft.com/en-us/cli/azure/ad/app?view=azure-cli-latest#az_ad_app_create
@@ -52,6 +55,10 @@ function New-AppRegistration {
         [switch] $NativeApp,
 
         [Parameter(Mandatory=$false)]
+        [ValidateSet("AzureADMyOrg","AzureADMultipleOrgs","AzureADandPersonalMicrosoftAccount","PersonalMicrosoftAccount")]
+        [string] $SignInAudience = "AzureADMyOrg",
+
+        [Parameter(Mandatory=$false)]
         [string] $IdentifierUris,
 
         [Parameter(Mandatory=$false)]
@@ -59,9 +66,6 @@ function New-AppRegistration {
 
         [Parameter(Mandatory=$false)]
         [switch] $AddSecret,
-
-        [Parameter(Mandatory=$false)]
-        [switch] $DisableImplicitGrantFlowOAuth2,
 
         [Parameter(Mandatory=$false)]
         [switch] $UseV2AccessTokens,
@@ -74,88 +78,158 @@ function New-AppRegistration {
 
         [Parameter(Mandatory=$false)]
         [ValidateSet("User","ManagedIdentity")]
-        [string] $AsAccountType = "User"
+        [string] $ConnectAs = "User"
     )
 
-    Write-Host "[+] Running under the context of a $AsAccountType"
-    # Validate signed in user
-    if ($AsAccountType == 'User'){
-        $signedInUser = az ad signed-in-user show --query '[displayName, mail]' | convertfrom-json
-        if (!($signedInUser)) {
-            az login
+    # Variables
+    $DeploymentScriptOutputs = @{}
+
+    # Processing current security context
+    Write-Host "[+] Running under the context of a $ConnectAs account"
+    $context = Get-AzContext
+    if (!$context) {
+        if ($ConnectAs == 'User') {
+            Connect-AzAccount
         }
         else {
-            Write-Host "[+] Using the following user context:"
-            Write-Host "[+] UserName: $($SignedInUser[0])"
-            Write-Host "[+] E-mail: $($SignedInUser[1])"
+            Connect-AzAccount -Identity
         }
     }
-    else {
-        $identity = az login --identity | ConvertFrom-Json
-        $identity
-    }
 
-    # Registering a new Azure AD Application
-    $registeredApp = $(az ad app list --query "[?displayName=='$Name']" --all | ConvertFrom-Json)[0]
+    # Get MS Graph access token
+    Write-Host "[+] Getting MS Graph raw acess token.."
+    $accessToken = (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com/").Token
+
+    # Set up HTTP headers
+    $Headers = @{}
+    $Headers["Authorization"] = "Bearer $accessToken"
+    $Headers["Content-Type"] = "application/json"
+
+    # Verify if application exists
+    $params = @{
+        "Method"  = "Get"
+        "Uri"     = "https://graph.microsoft.com/v1.0/applications?`$filter=displayName eq '$Name'"
+        "Headers" = $Headers
+    }
+    $registeredApp = ((Invoke-RestMethod @params).value)[0]
+
     if ($registeredApp){
         Write-Host "[!] Azure AD application $Name already exists!"
     }
     else {
+        # Registering new application
         Write-Host "[+] Registering new Azure AD application: $Name"
+        $body = @{ 
+            displayName = "$Name"
+            signInAudience = "$SignInAudience"
+            api = @{
+                oauth2PermissionScopes = @(
+                    @{
+                        id = [guid]::NewGuid()
+                        adminConsentDescription = "Allow the application to access $Name on behalf of the signed-in user."
+                        adminConsentDisplayName = "Access $Name"
+                        userConsentDescription = "Allow the application to access $Name on your behalf."
+                        userConsentDisplayName = "Access $Name"
+                        value = "user_impersonation"
+                        type = "Admin"
+                        isEnabled = $True
+                    }
+                )
+            }
+        }
         if ($NativeApp) {
-            $registeredApp = $(az ad app create --display-name $Name --native-app | ConvertFrom-Json)
+            $body["publicClient"] = @{
+                redirectUris = @("http://localhost")
+            }
         }
-        else {
-            $registeredApp = $(az ad app create --display-name $Name | ConvertFrom-Json)
+        $params = @{
+            "Method"  = "Post"
+            "Uri"     = "https://graph.microsoft.com/v1.0/applications"
+            "Headers" = $Headers
+            "Body"    = $body | ConvertTo-Json -Compress -Depth 20
         }
-
-        # Sleep
-        Start-Sleep -s 15
+        $registeredApp = Invoke-RestMethod @params
+        Start-Sleep -s 10
     }
+    $registeredApp
+    $DeploymentScriptOutputs['appName'] = $Name
+    $DeploymentScriptOutputs['appId'] = $registeredApp.AppId
 
     if ($IdentifierUris) {
-        Write-Host "[+] Updating $Name application: Adding unique identifier URIs that Azure AD can use for this app"
-        az ad app update --id $registeredApp.AppId --identifier-uris $IdentifierUris | Out-Null
-    }
-    if ($ReplyUrls) {
-        Write-Host "[+] Updating $Name application: Adding URIs to which Azure AD will redirect in response to an OAuth 2.0 request"
-        az ad app update --id $registeredApp.AppId --reply-urls $ReplyUrls | Out-Null
+        Write-Host "[+] Updating $Name application: Updating the URIs that identify the application within its Azure AD tenant."
+        $body = @{
+            identifierUris = @($IdentifierUris)
+        }
+        $params = @{
+            "Method"  = "Patch"
+            "Uri"     = "https://graph.microsoft.com/v1.0/applications/$($registeredApp.id)"
+            "Headers" = $Headers
+            "Body"    = $body | ConvertTo-Json -Compress
+        }
+        Invoke-RestMethod @params
     }
 
+    if (($ReplyUrls) -and !($NativeApp) ) {
+        Write-Host "[+] Updating $Name application: Updating URLs where user tokens are sent for sign-in"
+        $body = @{ 
+            web = @{
+                redirectUris = @($ReplyUrls)
+                implicitGrantSettings = @{
+                    enableIdTokenIssuance = $True
+                }
+            }
+        }
+        $params = @{
+            "Method"  = "Patch"
+            "Uri"     = "https://graph.microsoft.com/v1.0/applications/$($registeredApp.id)"
+            "Headers" = $Headers
+            "Body"    = $body | ConvertTo-Json -Compress -Depth 10
+        }
+        Invoke-RestMethod @params
+    }
     # Creating the new Azure AD application service principal
-    $spExists = $(az ad sp list --query "[?appDisplayName=='$Name']" --all  | ConvertFrom-Json)[0]
-    if ($spExists){
+    # Verify if service principal exists
+    Write-Host "[+] Creating a service principal mapped to the $Name application"
+    $params = @{
+        "Method"  = "Get"
+        "Uri"     = "https://graph.microsoft.com/v1.0/servicePrincipals?`$filter=displayName eq '$Name'"
+        "Headers" = $Headers
+    }
+    $appSP = ((Invoke-RestMethod @params).value)[0]
+    if ($appSP){
         Write-Host "[!] Azure AD application $Name already has a service principal"
     }
     else {
-        Write-Host "[+] Creating Azure AD service principal for $Name application"
-        az ad sp create --id $registeredApp.appId
-
+        $body = @{ 
+            appId = $registeredApp.appId
+        }
+        $params = @{
+            "Method"  = "Post"
+            "Uri"     = "https://graph.microsoft.com/v1.0/servicePrincipals"
+            "Headers" = $Headers
+            "Body"    = $body | ConvertTo-Json -Compress -Depth 10
+        }
+        $appSP = Invoke-RestMethod @params
+        $appSP
         # Sleep
-        Start-Sleep -s 15
+        Start-Sleep -s 10
     }
 
     #Add credentials to application
     if ($AddSecret) {
-        Write-Host "[+] Getting MS Graph access token with current security context"
-        $token=$(az account get-access-token --resource-type ms-graph --query accessToken --output tsv)
-        write-verbose "Using the following MS Graph token: $token"
-        $headers = @{
-            "Authorization" = "Bearer $token"
-            "Content-Type" = "application/json"
-        }
+        Write-Host "[+] Adding a secret to $Name application"
         $pwdCredentialName = 'CloudKatanaSecret'
         $body = @{
             passwordCredential = @{ displayName = "$($pwdCredentialName)" }
-        } | ConvertTo-Json -Compress
+        }
         $params = @{
             "Method"  = "Post"
-            "Uri"     = "https://graph.microsoft.com/v1.0/applications/$($registeredApp.objectId)/addPassword"
-            "Body"    = $body
-            "Headers" = $headers
+            "Uri"     = "https://graph.microsoft.com/v1.0/applications/$($registeredApp.id)/addPassword"
+            "Headers" = $Headers
+            "Body"    = $body | ConvertTo-Json -Compress
         }
-        Write-Host "[+] Adding credentials to $Name application"
         $credentials = Invoke-RestMethod @params
+
         if (!($credentials)){
             Write-Error "Error adding credentials to $Name"
             return
@@ -163,59 +237,74 @@ function New-AppRegistration {
         
         Write-Host "[+] Extracting secret text from results. Save it for future operations"
         $secret = $credentials.secretText
-    }
-
-    if ($DisableImplicitGrantFlowOAuth2){
-        Write-Host "[+] Updating $Name application: Disabling implicit grant flow for OAuth2"
-        az ad app update --id $registeredApp.AppId --set oauth2AllowIdTokenImplicitFlow=false *>&1 | Out-Null
+        $DeploymentScriptOutputs['appSecretText'] = $secret
+        $DeploymentScriptOutputs['appCredentialName'] = $pwdCredentialName
     }
 
     if ($UseV2AccessTokens){
+        Write-Host "[+] Updating $Name application: Setting application to use V2 access tokens"
         # Set application to use V2 access tokens
-        $Body = @{
+        $body = @{
             api = @{
                 requestedAccessTokenVersion = 2
             }
-        } | ConvertTo-Json -Compress | ConvertTo-Json
-        # Pipe to ConvertTo-Json twice to escape all quotes, or az cli will remove them when parsing
-        Write-Host "[+] Updating $Name application: Setting application to use V2 access tokens"
-        az rest --method PATCH --uri "https://graph.microsoft.com/v1.0/applications/$($registeredApp.objectId)" --body $Body --headers "Content-Type=application/json"
+        }
+        $params = @{
+            "Method"  = "Patch"
+            "Uri"     = "https://graph.microsoft.com/v1.0/applications/$($registeredApp.id)"
+            "Headers" = $Headers
+            "Body"    = $body | ConvertTo-Json -Compress
+        }
+        Invoke-RestMethod @params
     }
 
     if($RequireAssignedRole){
         Write-Host "[+] Updating $Name application: Setting application to require users being assigned a role "
-        az ad sp update --id $registeredApp.AppId --set appRoleAssignmentRequired=true | Out-Null
+        $body = @{
+            appRoleAssignmentRequired = $True
+        }
+        $params = @{
+            "Method"  = "Patch"
+            "Uri"     = "https://graph.microsoft.com/v1.0/servicePrincipals/$($appSP.Id)"
+            "Headers" = $Headers
+            "Body"    = $body | ConvertTo-Json -Compress
+        }
+
+        Invoke-RestMethod @params
         # Sleep
         Start-Sleep -s 10
     }
 
     if($AssignAppRoleToUser){
         Write-Host "[+] Granting app role assignment to $AssignAppRoleToUser "
-        $AppSp = az ad sp show --id $registeredApp.AppId | ConvertFrom-Json
-        $principalId = az ad user show --id $AssignAppRoleToUser --query 'objectId' -o tsv
+        Write-Host "    [>>] Getting user's principal ID"
+        $params = @{
+            "Method"  = "Get"
+            "Uri"     = "https://graph.microsoft.com/v1.0/users/$AssignAppRoleToUser"
+            "Headers" = $Headers
+        }
+        $principalId = (Invoke-RestMethod @params).Id
 
-        $Body = @{
+        Write-Host "    [>>] Adding user to application.."
+        $body = @{
             appRoleId = [Guid]::Empty.Guid
             principalId = $principalId
-            resourceId = $AppSp.objectId
-        } | ConvertTo-Json -Compress | ConvertTo-Json
+            resourceId = $AppSp.id
+        }
 
-        $AssignAppRoleResult = az rest --method post --uri "https://graph.microsoft.com/v1.0/users/$AssignAppRoleToUser/appRoleAssignments" --body $Body --headers "Content-Type=application/json"
+        $params = @{
+            "Method"  = "Post"
+            "Uri"     = "https://graph.microsoft.com/v1.0/users/$AssignAppRoleToUser/appRoleAssignments"
+            "Headers" = $Headers
+            "Body"    = $body | ConvertTo-Json -Compress
+        }
+
+        $AssignAppRoleResult = Invoke-RestMethod @params
         if (!$AssignAppRoleResult) {
             Write-Error "Error granting app role assignment to user $AssignAppRoleToUser"
             return
         }
     }
-
-    # Output the Application for use later
-    Write-Host "[+] Application: $Name"
-    $registeredApp
-    Write-Host "[+] Application ID: $($registeredApp.AppId)"
-
-    if ($AddSecret){
-        Write-Host "[+] New Client Secret: $pwdCredentialName "
-        $credentials
-        Write-Host "[+] Secret Text: "
-        $secret
-    }
+    Write-Host "[+] Additional Output: "
+    $DeploymentScriptOutputs
 }
