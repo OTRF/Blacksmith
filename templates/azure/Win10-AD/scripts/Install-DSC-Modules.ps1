6478aff266db489e618993c67736ac24a1d3bd39
Roberto Rodriguez <rrodri0622@gmail.com>
2022-02-06 23:59:15 -0500

    updated Win10-AD template to use nested Win10 template

--- templates/azure/Win10-AD/scripts/Install-DSC-Modules.ps1
+++ templates/azure/Win10-AD/scripts/Install-DSC-Modules.ps1
@@ -1,12 +1,5 @@
 #Requires -Version 5
 
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory)]
-    [ValidateSet("DC",'Endpoint')]
-    [string]$SetupType
-
-)
 Set-ExecutionPolicy Unrestricted -Force
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -19,7 +12,4 @@ Install-Module -Name NetworkingDsc -RequiredVersion 8.2.0
 Install-Module -Name xPSDesiredStateConfiguration -RequiredVersion 9.1.0
 Install-Module -Name ComputerManagementDsc -RequiredVersion 8.4.0
 
-if ($SetupType -ne 'Endpoint')
-{
-    Install-Module -Name xDnsServer -RequiredVersion 1.16.0.0
-}
+Install-Module -Name xDnsServer -RequiredVersion 2.0.0
--- templates/azure/Win10-AD/scripts/Set-Initial-Settings.ps1
+++ templates/azure/Win10-AD/scripts/Set-Initial-Settings.ps1
@@ -1,27 +1,23 @@
 # Author: Roberto Rodriguez (@Cyb3rWard0g)
 # License: GPL-3.0
 
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$false)]
-    [string]$SetupType
-)
-
 # Install DSC Modules
-& .\Install-DSC-Modules.ps1 -SetupType $SetupType
+& .\Install-DSC-Modules.ps1
 
 # Custom Settings applied
 & .\Prepare-Box.ps1
 
+# Additional configs
+& .\Disarm-Box.ps1
+
+# Additional Firewall rules
+& .\Disarm-Firewall.ps1
+
+# Enable PSRemoting
+& .\Configure-PSRemoting.ps1
+
 # Windows Security Audit Categories
-if ($SetupType -eq 'DC')
-{
-    & .\Enable-WinAuditCategories.ps1 -SetDC
-}
-else
-{
-    & .\Enable-WinAuditCategories.ps1
-}
+& .\Enable-WinAuditCategories.ps1 -SetDC
 
 # PowerShell Logging
 & .\Enable-PowerShell-Logging.ps1
