c616785daacefce84543af20c841243ae65d705c
Roberto Rodriguez <rrodri0622@gmail.com>
2022-04-18 22:30:14 -0400

    Template for Azure Automation Account

---
+++ templates/azure/Azure-Automation-Account/AzureAutomationTutorialNew.ps1
@@ -0,0 +1,34 @@
+<#
+    .DESCRIPTION
+        An example runbook which gets all the ARM resources using the Managed Identity
+
+    .NOTES
+        AUTHOR: Azure Automation Team
+        LASTEDIT: Oct 26, 2021
+#>
+
+"Please enable appropriate RBAC permissions to the system identity of this automation account. Otherwise, the runbook may fail..."
+
+try
+{
+    "Logging in to Azure..."
+    Connect-AzAccount -Identity
+}
+catch {
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+}
+
+#Get all ARM resources from all resource groups
+$ResourceGroups = Get-AzResourceGroup
+
+foreach ($ResourceGroup in $ResourceGroups)
+{    
+    Write-Output ("Showing resources in resource group " + $ResourceGroup.ResourceGroupName)
+    $Resources = Get-AzResource -ResourceGroupName $ResourceGroup.ResourceGroupName
+    foreach ($Resource in $Resources)
+    {
+        Write-Output ($Resource.Name + " of type " +  $Resource.ResourceType)
+    }
+    Write-Output ("")
+}
