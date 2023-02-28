5a57a738304c5b74e42041e2e88236f683415b98
Roberto Rodriguez <rrodri0622@gmail.com>
2023-02-28 15:18:15 -0500

    nginx test

---
+++ resources/scripts/docker/nginx/scripts/nginx-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Author: Roberto Rodriguez (@Cyb3rWard0g)
+# License: GPL-3.0
+
+_term() {
+  echo "Terminating Nginx Services"
+  service nginx stop
+  exit 0
+}
+trap _term SIGTERM
+
+# ************* Creating JupyterHub Certificate ***********
+openssl req \
+    -x509 \
+    -nodes \
+    -days 365 \
+    -newkey rsa:2048 \
+    -keyout /etc/ssl/private/Nginx.key \
+    -out /etc/ssl/certs/Nginx.crt \
+    -subj "/C=US/ST=VA/L=VA/O=Nginx/OU=Ngnix Nginx/CN=Nginx"
+
+echo "Starting remaining services.."
+service nginx restart
+
+echo "Pushing Nginx Logs to console.."
+tail -f /var/log/nginx/*.log
